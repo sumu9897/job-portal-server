eba4a89577eab787ea28b558d915d1eb2c29eef0
sumu9897 <sumon.bncd@gmail.com>
2024-12-19 13:39:00 +0600

    data find by applicant email

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
-const port = process.env.PORT ||  5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 app.use(cors());
@@ -17,32 +17,69 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@mern-es
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        // jobs relates APIs
+        const jobsCollection = client.db('jobPortal').collection('jobs');
+        const jobApplicationCollection = client.db('jobPortal').collection('job_applications')
+
+        app.get('/jobs', async (req, res) => {
+            const cursor = jobsCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/jobs/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id)}
+            const result = await jobsCollection.findOne(query);
+            res.send(result);
+        })
+
+        // job application api
+
+        app.get('/job-application', async(req ,res) => {
+            const email = req.query.email;
+            const query = {applicant_email : email}
+            const result = await jobApplicationCollection.find(query).toArray();
+            res.send(result)
+        })
+
+
+        app.post('/job-applications', async(req, res) => {
+            const application = req.body;
+            const result = await jobApplicationCollection.insertOne(application)
+            res.send(result);
+
+        })
+
+
+
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
-app.get('/',(req, res) => {
+app.get('/', (req, res) => {
     res.send('Boss Ami to Suru Hoiya gesi')
 })
 
-app.listen(port , () => {
+app.listen(port, () => {
     console.log(`Job is waiting at ${port}`)
 })
